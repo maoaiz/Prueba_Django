21272c82b7cf363c871ead8c9fd84a9cf4c6c4b2
Mauricio <mauricioaizaga@gmail.com>
2012-06-26 20:14:46 -0500

    Agregando url para static

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-#from django.conf import settings
+from django.conf import settings
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 from github.views import update
@@ -17,6 +17,7 @@ urlpatterns = patterns('',
 #     url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.MEDIA_ROOT,}),
     # Uncomment the next line to enable the admin:
      url(r'^admin/', include(admin.site.urls)),
+     url(r'^static/(.*)$', 'django.views.static.serve', {'document_root':settings.STATIC_ROOT}),
 )
 
 #if settings.DEBUG:
