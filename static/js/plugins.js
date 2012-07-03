470238ae6eac8a37f6ac5d0e9e1e42d1cd9e0cb2
Mauricio <mauricioaizaga@gmail.com>
2012-07-03 17:50:55 -0500

    Modificando el formulario de login

--- static/js/plugins.js
+++ static/js/plugins.js
@@ -4,7 +4,7 @@ function createTable(list,query){
     $("#search-info > #query-string").text(query);
     $("#movie").fadeIn(160,function(e){
         $("#movie > table").slideDown("slow")
-        });
+    });
     var l = '';
     for(i=0;i<list.length;i++){
         try{
@@ -25,86 +25,23 @@ function createTable(list,query){
         }
         var desc = ( list[i].overview != null ) ?(list[i].overview).substring(0, 400)+"...":"No Description";
         l += "<tr>" +
-            "<td valign='top' class='movie_image'>" +
-                "<a href='"+url+"' target='_blank'><img src='"+src+"' class='img50' /></a>" +
-            "</td>" +
-            "<td valign='top' class='title'>" +
-                "<a href='"+url+"' target='_blank'>"+list[i].name +
-                "<div>"+desc+"</div></a>" +
-            "</td>" +
-            "<td>"+
-                "<a href='"+url+"' class='bonton btn btn-success' target='_blank' >TMDB</a>"+
-                "<a href='"+url_imdb+"' class='bonton btn btn-warning' target='_blank' >IMDB</a>"+
-            "</td>"+
+        "<td valign='top' class='movie_image'>" +
+        "<a href='"+url+"' target='_blank'><img src='"+src+"' class='img50' /></a>" +
+        "</td>" +
+        "<td valign='top' class='title'>" +
+        "<a href='"+url+"' target='_blank'>"+list[i].name +
+        "<div>"+desc+"</div></a>" +
+        "</td>" +
+        "<td>"+
+        "<a href='"+url+"' class='bonton btn btn-success' target='_blank' >TMDB</a>"+
+        "<a href='"+url_imdb+"' class='bonton btn btn-warning' target='_blank' >IMDB</a>"+
+        "</td>"+
         "</tr>";
     }
     $("#movie > table").append(l);
 }
-function imdb1(url){
-    (function($) {
-        $.ajax(
-        {
-            type		: "GET",
-            url			: url,
-            async		: false,
-            //jsonpCallback: 'myFunction',
-            contentType	: 'application/json',
-            dataType	: 'jsonp',
-            //	data		: "id_project="+id,
-            beforeSend	: function(data){
-                $("#load").html('<i>Cargando...</i>');
-            },
-            success		: function(requestData){ 	//Llamada exitosa
-            //var movie = jQuery.parseJSON(requestData);
-            //$("#movie2").html("<pre>"+requestData.Title+"</pre>");
-            //$("#movie2").html("<pre>"+movie['Title']+" + "+movie['Year']+"</pre>");
-            },
-            error		: function(requestData, strError, strTipoError){
-                alert("Error: " + strTipoError +' => ' + strError+ ' , req: '+requestData);
-            },
-            complete	: function(requestData, exito){ 
-                $("#load").html("<strong>"+exito+"</strong>");
-            }
-        });
-    })(jQuery);
-}
 
-function imdb2(url){
-    //jQuery.getScript(url+ "&callback=myFunction",function(e){alert("callback");}	); //   + "&callback=?"
-    var res=jQuery.getScript(url+ "&callback=?",function(e){
-        alert("callback")
-    });
-/*
-	imdb$avengers = function (list) {
-		  //alert("llego a la funcion: "+ list.q);
-		  $("#movie").html("<table>");
-		  for(i=0;i<list.d.length;i++){
-			  try{
-			  	var src = list.d[i].i[0];
-			  }catch(Exception){
-				  //alert("hubo un error");
-				  src="http://i.media-imdb.com/images/mobile/film-40x54.png"
-			  }
-		  	$("#movie").append("<tr><td><img src='"+src+"' width='35px' /></td><td  valign='top'>"+list.d[i].l+"</td></tr>");
-		  }
-		  $("#movie").append("</table>");
-	};*/
-//$.getJSON(url).success(function(data){
-//    console.log(data); // will contain all data (and display it in console)
-//})
-
-// With local proxy to a PHP script replacing imdb$foo with a sanitized
-// version of $_GET['callback']
-//jQuery.getScript('imdb.php?q=foo&callback=myFunction');
-
-// With local proxy to a PHP script to fix the callback, using jQuery automatic
-// callback magic '?' value
-//jQuery.getJSON( 'imdb.php?q=foo&callback=?', function (list) {
-
-//});
-}
-
-function imdb3(url,q){
+function getTMDB(url,q){
     (function($) {
         $.ajax({
             type: 'GET',
@@ -127,7 +64,7 @@ function imdb3(url,q){
                     $("#search-info > #query-string").text(q);
                     $("#movie").fadeIn(160,function(e){
                         $("#movie > table").slideDown("slow")
-                        });
+                    });
                 }
                 else{
                     createTable(json,q);
@@ -144,22 +81,54 @@ function imdb3(url,q){
     })(jQuery);
 }
 
-function main(){
-    //    var url = "http://sg.media-imdb.com/suggests/"+q0+"/"+q+".json";
-    //    var url2 = "http://www.imdb.com/xml/find?json=1&nr=1&nm=on&q="+q;
-    //    var url3 = "http://www.imdb.com/xml/find?xml=1&tt=1&nm=on&q="+q;
-    //    var url6="http://api.themoviedb.org/2.1/Movie.search/es-CO/xml/"+APIKEY+"/"+q;
-    //    var url7="http://api.themoviedb.org/2.1/Movie.search/es-CO/json/"+APIKEY+"/"+q;
-    $("#search-form").on("submit",search);
-}
-
 function search(e){
     e.preventDefault();
     var q=$("#search").val();
     var APIKEY = "10562c9ee2722c0be2a1c3bc31e3028a";
     var url7="http://api.themoviedb.org/2.1/Movie.search/es-CO/json/"+APIKEY+"/"+q;
 
-    imdb3(url7,q);
+    getTMDB(url7,q);
+}
+
+function main(){
+    //    var url = "http://sg.media-imdb.com/suggests/"+q0+"/"+q+".json";
+    //    var url2 = "http://www.imdb.com/xml/find?json=1&nr=1&nm=on&q="+q;
+    //    var url3 = "http://www.imdb.com/xml/find?xml=1&tt=1&nm=on&q="+q;
+    //    var url6="http://api.themoviedb.org/2.1/Movie.search/es-CO/xml/"+APIKEY+"/"+q;
+    //    var url7="http://api.themoviedb.org/2.1/Movie.search/es-CO/json/"+APIKEY+"/"+q;
+    
+    //--------<on submit when search>------------//
+    $("#search-form").on("submit",search);
+    //--------</on submit when search>------------//
+    
+    //-------------------<letra blanca del perfil>-------------------------//
+    $("ul.dropdown-menu li.current-user").hover(
+        function(e){$(".content-box p, .content-box small").addClass("color-fff")},
+        function(e){$(".content-box p, .content-box small").removeClass("color-fff")}
+    );
+    //-------------------</letra blanca del perfil>-------------------------//
+    
+    //------<No cierra el Menú de login al dar click>---------------/
+    $(".next-li").focus(function(e){
+            $("ul.dropdown-menu").addClass("disblock")
+        }).blur(function(e){
+           // $("ul.dropdown-menu").removeClass("disblock")
+        });
+    //------ </No cierra el Menú de login al dar click>--------------/
+    
+    
+    //------ <On Login Submit >--------------/
+    $("#close-login-menu").on("click",function(e){
+        e.preventDefault();
+        $("ul.dropdown-menu").removeClass("disblock");
+    });
+//    $(".btn-group").on("click",function(e){
+//        e.preventDefault();
+//        if($("#menu-login").hasClass("open"))
+//            $("ul.dropdown-menu").removeClass("disblock");
+//        
+//    });
+    //------ </On Login Submit >--------------/
 }
 
 $(document).on("ready",main);
--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def index(request):
-    return render_to_response('index.html', None)
+    return render_to_response('index.html', None, context_instance = RequestContext(request))
